85c2636eadeb317f89190471256b8cb3f2ca03a7
Tyreese Burton <djoreally@gmail.com>
2026-09-01 17:43:21 -0400

    feat: expose Fleet Account 360 endpoint

--- src/server/routes/customerParts.ts
+++ src/server/routes/customerParts.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { operationsDataService } from '../services/operationsData.js';
+import { fleetAccount360Service } from '../services/fleetAccount360.js';
 import { FleetAuthError, fleetAuthFailure, requireFleetOrganization } from '../services/fleetAuth.js';
 
 export const customerPartsRouter = Router();
@@ -10,6 +11,7 @@ const fail = (res: any, error: unknown) => {
   return res.status(/not found/i.test(message) ? 404 : /required|must|numeric/i.test(message) ? 400 : 500).json({ error: message });
 };
 customerPartsRouter.get('/customers', async (req,res)=>{try{const org=await requireFleetOrganization(req);return res.json({customers:await operationsDataService.listCustomers(org,String(req.query.search??''))})}catch(e){return fail(res,e)}});
+customerPartsRouter.get('/customers/:id/overview', async (req,res)=>{try{const org=await requireFleetOrganization(req);return res.json({account:await fleetAccount360Service.get(org,req.params.id)})}catch(e){return fail(res,e)}});
 customerPartsRouter.post('/customers', async (req,res)=>{try{const org=await requireFleetOrganization(req);return res.status(201).json({customer:await operationsDataService.createCustomer(org,req.body??{})})}catch(e){return fail(res,e)}});
 customerPartsRouter.put('/customers/:id', async (req,res)=>{try{const org=await requireFleetOrganization(req);return res.json({customer:await operationsDataService.updateCustomer(org,req.params.id,req.body??{})})}catch(e){return fail(res,e)}});
 customerPartsRouter.delete('/customers/:id', async (req,res)=>{try{const org=await requireFleetOrganization(req);return res.json({deleted:await operationsDataService.deleteCustomer(org,req.params.id)})}catch(e){return fail(res,e)}});
